6668145b9071fa28bc9fdd74de2963cb0ceef0de
Vlad Băilescu <bailescu@adobe.com>
2018-12-21 12:55:14 +0200

    Created a POC for a new wcm container component
    * Added container items
    * Excluded impl from coverage checks (TODO: add proper testing)

--- bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/models/v1/GenericContainerImpl.java
+++ bundles/core/src/main/java/com/adobe/cq/wcm/core/components/internal/models/v1/GenericContainerImpl.java
@@ -15,47 +15,19 @@
  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 package com.adobe.cq.wcm.core.components.internal.models.v1;
 
-import javax.inject.Inject;
 import javax.annotation.PostConstruct;
-import java.util.ArrayList;
-import java.util.List;
 
-import com.adobe.cq.export.json.ComponentExporter;
-import com.adobe.cq.export.json.ContainerExporter;
-import com.adobe.cq.wcm.core.components.models.Carousel;
-import com.adobe.cq.wcm.core.components.models.GenericContainer;
-import com.day.cq.commons.DownloadResource;
-import com.day.cq.commons.jcr.JcrConstants;
-import com.day.cq.wcm.api.components.Component;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
-import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.injectorspecific.ScriptVariable;
-import org.apache.sling.models.annotations.injectorspecific.Self;
 import org.apache.sling.api.resource.ValueMap;
+import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
-@Model(adaptables = SlingHttpServletRequest.class, adapters = GenericContainer.class, resourceType = GenericContainerImp.RESOURCE_TYPE)
-public class GenericContainerImp extends AbstractImageDelegatingModel implements GenericContainer{
+import com.adobe.cq.wcm.core.components.models.GenericContainer;
 
+@Model(adaptables = SlingHttpServletRequest.class, adapters = GenericContainer.class, resourceType = GenericContainerImpl.RESOURCE_TYPE)
+public class GenericContainerImpl extends AbstractContainerImpl implements GenericContainer {
     public static final String RESOURCE_TYPE = "core/wcm/components/container/v1/container";
-
-    private final List<String> hiddenImageResourceProperties = new ArrayList<String>() {{
-        add(JcrConstants.JCR_TITLE);
-        add(JcrConstants.JCR_DESCRIPTION);
-    }};
-
-    @ScriptVariable
-    private Component component;
-
-    @ScriptVariable
-    private ValueMap properties;
-
-    @Self
-    private SlingHttpServletRequest request;
-
-    @Inject
-    protected Resource resource;
 
     private String backgroundImageSrc;
 
@@ -72,12 +44,8 @@ public class GenericContainerImp extends AbstractImageDelegatingModel implements
     private void initModel() {
         if(backgroundType != null && backgroundType.equals("backgroundImage"))
         {
-            String fileReference = properties.get(DownloadResource.PN_REFERENCE, String.class);
-
-            setImageResource(component, request.getResource(), hiddenImageResourceProperties);
-
-            //ValueMap properties = resource.getValueMap();
-            //String fileReference = properties.get("fileReference", String.class);
+            ValueMap properties = resource.getValueMap();
+            String fileReference = properties.get("fileReference", String.class);
             if(fileReference != null && !fileReference.isEmpty())
             {
                 backgroundImageSrc = fileReference;
@@ -90,7 +58,6 @@ public class GenericContainerImp extends AbstractImageDelegatingModel implements
                     backgroundImageSrc = fileResource.getPath();
                 }
             }
-
         }
 
     }
